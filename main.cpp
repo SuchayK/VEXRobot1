bd678b8975258dcf74b2e320d531408b6846772a
SuchayK <67595987+SuchayK@users.noreply.github.com>
2024-06-29 19:58:36 -0500

    PID Test #1

--- main.cpp
+++ main.cpp
@@ -1,104 +1,92 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Competition Template                                      */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
-//#include "image.h"
 using namespace vex;
 
-// A global instance of competition
 competition Competition;
 
-// define your global instances of motors and other devices here
+void moveForward(double x) {
+  right_drive.spinFor(fwd, x, turns, false);
+  left_drive.spinFor(fwd, x, turns);
+}
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+void moveForward(double x, bool z) {
+  right_drive.spinFor(fwd, x, turns, false);
+  left_drive.spinFor(fwd, x, turns, z);
+}
 
-void moveForward(double x){
-  right_drive.spinFor(fwd, x,turns,false);
-  left_drive.spinFor(fwd, x,turns);
+void moveBack(double x) {
+  right_drive.spinFor(reverse, x, turns, false);
+  left_drive.spinFor(reverse, x, turns);
 }
-void moveForward(double x, bool z){
-  right_drive.spinFor(fwd, x,turns,false);
-  left_drive.spinFor(fwd, x,turns, z);
-}
-void moveBack(double x){
-  right_drive.spinFor(reverse, x,turns,false);
-  left_drive.spinFor(reverse, x,turns);
-}
-void moveBack(double x, bool z){
-  right_drive.spinFor(reverse, x,turns,false);
-  left_drive.spinFor(reverse, x,turns, z );
-}
-void turnLeft(double x){
-  left_drive.spinFor(reverse,x,turns,false);
-  right_drive.spinFor(fwd,x,turns);
 
+void moveBack(double x, bool z) {
+  right_drive.spinFor(reverse, x, turns, false);
+  left_drive.spinFor(reverse, x, turns, z);
 }
-void turnLeft(double x, bool z){
-  left_drive.spinFor(reverse,x,turns,false);
-  right_drive.spinFor(fwd,x,turns, z);
+
+void turnLeft(double x) {
+  left_drive.spinFor(reverse, x, turns, false);
+  right_drive.spinFor(fwd, x, turns);
 }
-void turnRight(double x){
-  left_drive.spinFor(fwd,x,turns,false);
-  right_drive.spinFor(reverse,x, turns);
-} 
 
-void rightDegrees(double x){
-  left_drive.spinFor(fwd,x,degrees,false);
-  right_drive.spinFor(reverse,x, degrees);
-} 
+void turnLeft(double x, bool z) {
+  left_drive.spinFor(reverse, x, turns, false);
+  right_drive.spinFor(fwd, x, turns, z);
+}
 
-void turnRight(double x, bool z){
-  left_drive.spinFor(fwd,x,turns,false);
-  right_drive.spinFor(reverse,x, turns, z);
+void turnRight(double x) {
+  left_drive.spinFor(fwd, x, turns, false);
+  right_drive.spinFor(reverse, x, turns);
+}
+
+void turnRight(double x, bool z) {
+  left_drive.spinFor(fwd, x, turns, false);
+  right_drive.spinFor(reverse, x, turns, z);
+}
+
+void rightDegrees(double x) {
+  left_drive.spinFor(fwd, x, degrees, false);
+  right_drive.spinFor(reverse, x, degrees);
+}
+
+void leftDegrees(double x) {
+  left_drive.spinFor(reverse, x, degrees, false);
+  right_drive.spinFor(fwd, x, degrees);
+}
+
+void moveForwardPID(double targetDistance) {
+  double kP = 0.5;
+  double error = targetDistance;
+  double motorPower;
+  
+  while (fabs(error) > 0.1) {
+    error = targetDistance - left_drive.position(turns);
+    motorPower = error * kP;
+    left_drive.spin(fwd, motorPower, pct);
+    right_drive.spin(fwd, motorPower, pct);
+    wait(20, msec);
+  }
+  left_drive.stop();
+  right_drive.stop();
 }
 
 void pre_auton(void) {
-  // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
 
   right_drive.setStopping(brake);
   left_drive.setStopping(brake);
   right_drive.setVelocity(40, pct);
-  left_drive.setVelocity(40, pct);  
-
+  left_drive.setVelocity(40, pct);
 }
 
 void autonomous(void) {
-  
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
-  
-  //offside
-  
   moveForward(4.376, false);
   wait(2, sec);
-  moveBack(3.891,false);
+  moveBack(3.891, false);
   wait(2, sec);
   turnRight(3.42);
   wait(2, sec);
   turnLeft(3.42);
-
   rightDegrees(85.1);
-
+  leftDegrees(85.1);
+  moveForwardPID(10.0);
 }
